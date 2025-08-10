7345ec089f4f1449d992271d79c6f0daf96af1b9
Yucheng-An <yuc.an2019@gmail.com>
2025-08-10 14:43:24 -0700

    Finished Backend basic files

---
+++ apps/web/idealhub/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+export default App;
