274296ddde70f058b06445bda7c9242eb7756dc7
Yucheng-An <yuc.an2019@gmail.com>
2025-08-13 13:21:57 -0700

    Format code

--- apps/web/idealhub/src/components/TodoList.jsx
+++ apps/web/idealhub/src/components/TodoList.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {getAll} from '../services/todoListApi.js'
+
 const USERID = 1
+
 function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
-  const [allTodos,setAllTodos] = useState([])
-  const handleGetAllTodos= async (userId) =>{
-      // Set userId = 1
-      const Id = 1
-      return setAllTodos(await getAll(Id))
-      // setAllTodos(result)
-      // result.map((task) => {console.log(task)})
-  }
-  return (
-    <div>
-      <p className="font-mono font-medium text-red-500"> Todo List</p>
-      <input className="border-2 border-gray-300 rounded-md p-2" type="text" placeholder="Add a new todo" />
-      <button className="bg-orange-500 text-white p-2 rounded-md">Add</button>
-        <button className="bg-orange-500 text-white p-2 rounded-md" onClick = {handleGetAllTodos}>show all</button>
-      <ul className="list-disc">
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState("");
+    const [allTodos, setAllTodos] = useState([])
+
+    const handleGetAllTodos = async (userId) => {
+        // Set userId = 1
+        const Id = 1
+        return setAllTodos(await getAll(Id))
+        // setAllTodos(result)
+        // result.map((task) => {console.log(task)})
+    }
+    return (
+        <div>
+            <p className="font-mono font-medium text-red-500"> Todo List</p>
+            <input className="border-2 border-gray-300 rounded-md p-2" type="text" placeholder="Add a new todo"/>
+            <button className="bg-orange-500 text-white p-2 rounded-md">Add</button>
+            <button className="bg-orange-500 text-white p-2 rounded-md" onClick={handleGetAllTodos}>show all</button>
+            <ul className="list-disc">
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default TodoList;
--- apps/web/idealhub/src/services/todoListApi.js
+++ apps/web/idealhub/src/services/todoListApi.js
@@ -3,8 +3,6 @@ const baseUrl = 'http://localhost:3000/api/todolist'
 
 export const getAll = async (id) =>{
     const url = `${baseUrl}/${id}`
-    // console.log(url)
     const response = await axios.get(url)
-    // console.log(response.data)
     return response.data
 }
